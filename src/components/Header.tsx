b95e7c56bedafced8b3496c6c2132314e128f447
zocsen <zocsen@gmail.com>
2023-07-04 17:48:34 +0200

    Added Hero, made some header changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 function Header() {
 	return (
 		<nav className="header-navigation">
-			<h1 className="header-logo">ATEXPLASTIK</h1>
+			<a href="/"><h1 className="header-logo">ATEXPLASTIK</h1></a>
 			<div className="middle-navigation">
-				<a href="##" className="header-nav-item active hover-underline-animation">Kezdőlap</a>
+				<a href="/" className="header-nav-item active hover-underline-animation">Kezdőlap</a>
 				<a href="#products" className="header-nav-item hover-underline-animation">Termékek</a>
 				<a href="#about" className="header-nav-item hover-underline-animation">Rólunk</a>
 			</div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,16 +2,17 @@ function Hero() {
 	return <div className="hero-section">
 		<div className="hero-left">
 			<h1 className="main-slogan">
-				Biztonságos és minőségorientált megoldások a műanyaggyártásban.
+				Biztonságos és <br /> minőségorientált <br /> megoldások a <br /> műanyaggyártásban.
 			</h1>
 			<p>Szorosan együttműködünk ügyfeleinkkel az innovációs újítások érdekében!</p>
 			<div className="hero-buttons">
-				<button className="offer-button">Ajánlatkérés</button>
-				<button className="products-button">Termékek</button>
+				<a href="#contact" className="offer-button">Ajánlatkérés</a>
+				<a href="#products" className="products-button">Termékek</a>
+				
 			</div>
 		</div>
 		<div className="hero-right">
-			<img src={window.location.origin + "/images/manufacturing-cycle.svg"} alt="Hero" />
+			<img src={process.env.PUBLIC_URL + "/images/manufacturing-cycle.svg"} alt="Hero" />
 		</div>
 	</div>
 }
