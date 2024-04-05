3c02a60e39d10bd5a3df77beb060459e34bc5b93
Thief806 <143255752+Thief806@users.noreply.github.com>
2024-04-05 15:29:39 +0200

    Update CustomProduct.jsx

--- src/components/CustomProduct/CustomProduct.jsx
+++ src/components/CustomProduct/CustomProduct.jsx
@@ -12,7 +12,7 @@ function CustomProduct() {
     return <div id="customproduct" className="custom-product-detail">
         <img className="custom-product-image" src={process.env.PUBLIC_URL + "/images/tools.png"} alt="Tools" />
         <div className="custom-product-info">
-            <h2 className="custom-product-title">Személyre szabás2</h2>
+            <h2 className="custom-product-title">Személyre szabás most</h2>
             <p className="custom-product-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis nulla atque dolores omnis ad, excepturi iure cupiditate ut quaerat soluta vero fuga. Quae alias dicta quam, dolorum modi facilis blanditiis? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis distinctio sunt voluptatibus est repudiandae quo suscipit officia debitis, odit quibusdam ab beatae error sapiente voluptates rerum. Delectus quaerat quam totam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime eveniet veritatis animi minima id blanditiis culpa quam, nemo ratione.</p>
         </div>
     </div>
