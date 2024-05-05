6f2d5a8d548e261547c2131e25adfb3a0ae717d9
Thief806 <143255752+Thief806@users.noreply.github.com>
2024-05-05 22:19:25 +0200

    carousel

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { products } from "../Products/Products";
 import { useParams } from "react-router-dom";
-import 'react-slideshow-image/dist/styles.css'
-import { Slide } from 'react-slideshow-image'
 
 const ProductDetail = () => {
   const { id } = useParams();
@@ -14,14 +12,10 @@ const ProductDetail = () => {
   return (
     <div className="product-detail">
       <div className='product-imageslider'>
-        <div className="prev"></div>
-        <div className="slide-panel">
-          {ImageList.map(image => {return(<img src={image} /> )})}
-        </div>
-        <div className="next"></div>
         src={process.env.PUBLIC_URL + product.image}
         alt={product.name}
       </div>
+
       <div className="product-info">
         <h2 className="product-name">{product.name}</h2>
         {product.details.map((detail, index) => (
