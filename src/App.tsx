07e5d30e96997a4d997e3170496ba88515d66c6e
Thief806 <143255752+Thief806@users.noreply.github.com>
2024-05-13 00:02:59 +0200

    reviews commented

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
               {/* <SupportedAreas /> */}
               <CategorySegment />
               <CustomProduct />
-              <Reviews />
+              {/*<Reviews /> */}
             </>
           }
         />
